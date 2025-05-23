d9b9a95777198f6bb3ea7307c665baf6bd77b425
Mberekpelc <mblaw73@yahoo.com>
2025-05-23 14:33:24 +0100

    Create test_alert.py

---
+++ test_alert.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+global url
+url = "https://www.selenium.dev/documentation/webdriver/interactions/alerts/"
+
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(10)
+driver.get(url)
+element = driver.find_element(By.LINK_TEXT, "See an example alert")
+element.click()
+
+wait = WebDriverWait(driver, timeout=5)
+alert = wait.until(lambda d : d.switch_to.alert)
+text = alert.text
+alert.accept()
+assert text == "Sample alert"
+driver.quit()
+
+
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(10)
+driver.get(url)
+element = driver.find_element(By.LINK_TEXT, "See a sample confirm")
+driver.execute_script("arguments[0].click();", element)
+
+wait = WebDriverWait(driver, timeout=5)
+alert = wait.until(lambda d : d.switch_to.alert)
+text = alert.text
+alert.dismiss()
+assert text == "Are you sure?"
+
+driver.quit()
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(10)
+driver.get(url)
+element = driver.find_element(By.LINK_TEXT, "See a sample prompt")
+driver.execute_script("arguments[0].click();", element)
+
+wait = WebDriverWait(driver, timeout=5)
+alert = wait.until(lambda d : d.switch_to.alert)
+alert.send_keys("Selenium")
+text = alert.text
+alert.accept()
+assert text == "What is your tool of choice?"
+    
+driver.quit()
