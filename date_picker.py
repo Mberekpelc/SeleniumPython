5537daa6608a843cad89e03b5e82d0be8fceeba5
Lawrence <mblaw73@gmail.com>
2025-06-02 11:37:51 +0100

    added more test case

--- date_picker.py
+++ date_picker.py
@@ -2,7 +2,9 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import  Service
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
 Service_obj =Service("/Users/Lawrence.Mberekpe/Workspace/Drivers/chromedriver")
 driver = webdriver.Chrome(service=Service_obj)
@@ -46,4 +48,29 @@ for date in dates:
         date.click()
         break
 time.sleep(3)
+driver.quit()
+
+## Managing selection of date by dropdown
+
+Service_obj =Service("/Users/Lawrence.Mberekpe/Workspace/Drivers/chromedriver")
+driver = webdriver.Chrome(service=Service_obj)
+driver.get("https://www.dummyticket.com/dummy-ticket-for-visa-application/")
+driver.maximize_window()
+myWait = WebDriverWait(driver, 5)
+dateInput = myWait.until(EC.element_to_be_clickable((By.XPATH, "//input[@id='dob']")))
+dateInput.click()
+monthPicker = Select(driver.find_element(By.XPATH, "//select[@aria-label='Select month']"))
+monthPicker.select_by_visible_text("Dec")
+yearPicker = Select(driver.find_element(By.XPATH, "//select[@aria-label='Select year']"))
+yearPicker.select_by_visible_text("1990")
+
+#driver.find_element(By.LINK_TEXT, "20").click()
+
+allDates = driver.find_elements(By.XPATH, "//div[@id='ui-datepicker-div']//table/tbody/tr/td/a")  
+for date in allDates:
+    if date.text == "25":
+        date.click()
+        break
+    
+time.sleep(6)
 driver.quit()
